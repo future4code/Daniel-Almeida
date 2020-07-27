df5702b994053e02c9f7b64a26cb579026d5f8d6
vianadaniel <vianadaniel@hotmail.com>
2020-07-27 13:24:59 -0300

    acabei

---
+++ semana3/variaveis/src/index.js
@@ -0,0 +1,42 @@
+// Exercícios 1
+
+// b = 10
+
+// a = 10 b = 5
+
+// Exercício 2
+
+// 10, 20, 10
+
+let nome
+let idade
+
+console.log(typeof(nome), typeof(idade))
+
+nome = prompt("Qual seu nome?")
+idade = prompt("Qual sua idade")
+
+console.log(nome, idade)
+
+let perguntaUm = prompt("Qual seu endereço?")
+let perguntaDois = prompt("Qual seu telefone?")
+let perguntaTres = prompt("Qual seu cpf?")
+let perguntaQuatro = prompt("Qual seu cep?")
+let perguntaCinco = prompt("Qual seu bairro?")
+
+console.log(perguntaUm, perguntaDois, perguntaTres, perguntaQuatro, perguntaCinco)
+
+let comida = ['arroz', 'feijão', 'carne', 'macarrão', 'maçã']
+
+console.log(comida)
+
+console.log("Essas são minhas comidas preferidas:", comida)
+
+let preferida = prompt("Qual sua comida preferida?")
+console.log("Sua comida preferida é", comida[1])
+
+let pergunta = ["Você está com fome?", "Você está com camiseta azul?", "Você está com carro funcionando?"]
+let resposta = [true, false]
+
+console.log(pergunta[0], resposta[0], pergunta[1], resposta[0], pergunta[2], resposta[1])
+
