efeac66589517e61e235fa106d062cdc499faaf3
vianadaniel <vianadaniel@hotmail.com>
2020-08-03 16:43:30 -0300

    fim

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,92 @@
+// Exercício 1
+
+//a. 10 e 50 
+//b. Nada
+
+// Exercicio 2
+
+//a. Darvas e Goli
+//b. Iria imprimir todos
+
+// Exercicio 3
+
+// multiplicaNumeroParComEleMesmo Multiplica os números pares por ele mesmo criando um array novo com esses elementos
+
+// Exercicio 4
+
+// function me(){
+//     console.log("Eu sou Daniel, tenho 35 anos, moro em Minas Gerais e sou estudante")
+// }
+
+// function dados(name, age, address, condition){
+//     return console.log("Eu sou " + name + ", tenho " + age + ", moro em " + address + ", " + condition + " estudante.")
+
+// }
+// dados("daniel", 35, "Mg","sou")
+
+// Exercício 5
+
+// function soma(a, b){
+//     return console.log(a + b)
+// }
+// soma(10, 20)
+
+// function maior(a, b){
+//     a = prompt("Digite um numero")
+//     b = prompt("Digite um numero")
+//     if(a > b){
+//         return console.log("Primeiro numero é maior")
+//     } else if(b > a){
+//         return console.log("Primeiro numero é menor")
+//     }
+// }
+// maior()
+
+// function msg(a){
+//     for (let i=0; i< a; i++){
+//         console.log("Não é permitido escrever no console")
+//     }
+// }
+// msg(10)
+
+// Exercício 6
+
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+// function tamanho(a){
+// return console.log(a.length)
+// }
+// tamanho(array)
+
+// function sePar(a){
+//     if (a % 2 === 0){
+//         return true
+//     }
+    
+// }
+
+// function par(a){
+//     for (var i = 0; i <a.length; i++){
+//         let pares = []
+//         if (a[i] % 2 === 0){
+//             pares.push(a[i])
+//         }
+//         console.log(pares)
+//     }
+// console.log(a.length)
+// }
+// par(array)
+
+// function par(a){
+//     for (var i = 0; i <a.length; i++){
+//         let pares = []
+//         if (sePar(a[i])){
+//             pares.push(a[i])
+//         }
+//         console.log(pares)
+
+//     }
+// console.log(a.length)
+// }
+// par(array)
+
