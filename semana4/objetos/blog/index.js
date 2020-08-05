838cec64b64230d795191750f1d1c541db94f6a2
vianadaniel <vianadaniel@hotmail.com>
2020-08-05 16:35:02 -0300

    com desafio

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,12 +1,26 @@
+let arrayTotal = []
 function salvar(){
-    var titulo = document.getElementById('titulo-post').value
-    console.log(titulo)
-    const autor = document.getElementById('autor-post').value
-    console.log(autor)
-    const conteudo = document.getElementById('conteudo-post').value
-    console.log(conteudo)
-    const img = document.getElementById('img-post').value
-    const posts = document.getElementById('container-de-posts')
-    posts.innerHTML = `<h1>${titulo}</h1>` + `<h3>${autor}</h3>` + `<img style="width: 200px" src="${img}"></img>` + `<p>${conteudo}</p>`
+    let titulo = document.getElementById('titulo-post').value
     
+    let autor = document.getElementById('autor-post').value
+    
+    let conteudo = document.getElementById('conteudo-post').value
+   
+    let img = document.getElementById('img-post').value
+    
+    let post = {
+        titulo: titulo,
+        autor: autor,
+        imagem: img,
+        conteudo: conteudo
+    }
+    arrayTotal.push(post)
+    console.log(arrayTotal)
+    const posts = document.getElementById('container-de-posts')
+    posts.innerHTML += `<h1>${post.titulo}</h1>` + `<h3>${post.autor}</h3>` + `<img style="width: 200px" src="${img}"></img>` + `<p>${conteudo}</p>`
+    document.getElementById('titulo-post').value = ''
+    document.getElementById('autor-post').value = ''
+    document.getElementById('conteudo-post').value = ''
+    document.getElementById('img-post').value = ''
+
 }
