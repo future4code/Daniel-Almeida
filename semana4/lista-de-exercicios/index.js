799a046ae418224010ca6c8ca8c13a0f3880f53d
vianadaniel <vianadaniel@hotmail.com>
2020-08-07 13:54:29 -0300

    primeiro

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+// Interpretação de códigos
+
+// 1. Inseri o valor da contação do dollar cria uma constante meuDinheiro com o valor atribuido dentro da função
+
+// 2. Nesta função pede 2 paramatros o primeiro é o tipo de investimento e o segungo é o valor investido
+// cria duas constantes e imprime no console
+
+// 3. Imprime a quantidade de numeros no array, imprime o numero de pares e imprime o numero de impars
+
+// 4. Imprime o numero menor e o outro o maior
+
