6c92042bfde637a6cda73fb842bf07a0a5ffe973
vianadaniel <vianadaniel@hotmail.com>
2020-08-07 14:27:50 -0300

    segundo

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -9,3 +9,51 @@
 
 // 4. Imprime o numero menor e o outro o maior
 
+// Lógica de programação
+
+// 1. 
+
+// for (let i = 0; i < array.length; i++){}
+
+// for (let objeto of array){}
+
+// array.foreach(function)
+
+//2. a. false b. true c. false d. true e. false
+
+// 3. faltou i++ pra não dar loop infinito
+
+// 4.
+
+// let a, b, c
+// if (a === b || a === c || b === c){
+//     return console.log("Isóceles")
+// } else if(a === b && b === c){
+//     return console.log("Equilátero")
+// }else {
+//     return console.log("Escaleno")
+// }
+
+// 5.
+
+// let a = prompt("digite um numero")
+// let b = prompt("digite um numero")
+// if(a > b){
+//     console.log(a, "é maior que", b)
+// }else{
+//     console.log(b, "é maior que", a)
+// }
+// if (a % b === 0){
+//     console.log(a, "é divisivel por", b)
+// } else{
+//     console.log( a, "não é divisivel por", b)
+// }
+
+// if (b % a === 0){
+//     console.log(b, "é divisivel por", a)
+// } else{
+//     console.log(b, "não é divisivel por", a)
+// }
+
+// console.log("A diferença entre eles é", (a - b))
+
