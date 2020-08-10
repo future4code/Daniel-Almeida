8a6997ba29f764e4fc75027975a12ba0cce92187
vianadaniel <vianadaniel@hotmail.com>
2020-08-10 08:02:29 -0300

    quinto

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -164,17 +164,17 @@
 
 // 3. 
 
-const pessoas = [
-	{ nome: "Paula", idade: 12, altura: 1.8},
-	{ nome: "João", idade: 20, altura: 1.3},
-	{ nome: "Pedro", idade: 15, altura: 1.9},
-	{ nome: "Luciano", idade: 22, altura: 1.8},
-	{ nome: "Artur", idade: 10, altura: 1.2},
-	{ nome: "Soter", idade: 70, altura: 1.9}
-]
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
 
-const callback = (permitido, index, array) => {
-         return permitido.idade > 14 && permitido.altura > 1.5 
-}
-const permite = pessoas.filter(callback)
-console.log(permite)
+// const callback = (permitido, index, array) => {
+//          return permitido.idade > 14 && permitido.altura > 1.5 
+// }
+// const permite = pessoas.filter(callback)
+// console.log(permite)
