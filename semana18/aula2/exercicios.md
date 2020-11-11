c06383777630fa5e62038ea2f14d9f0dfccd922f
Daniel Viana <vianadaniel@hotmail.com>
2020-11-11 16:26:40 -0300

    fim

--- semana18/aula2/exercicios.md
+++ semana18/aula2/exercicios.md
@@ -4,3 +4,4 @@ que é assíncrona.
  Só que ela precisa de um salt. Este é gerado a partir da função genSalt que espera receber o cost, ou, como a lib bcryptjs chama, os rounds.
 
 ### Exercício 2
+a. Primeiro o cadastro para salvar a senha já criptografada 
---
+++ semana18/aula3/src/endpoints/getAddressInfo.ts
@@ -0,0 +1,20 @@
+import { Request, response, Response } from "express";
+import { getAddress } from "../services/getAddressInfo";
+
+export default async function getAddressByCep(
+   req: Request,
+   res: Response
+) {
+   try {
+      const address = await getAddress(Number(req.params.cep))
+
+      if (!address) {
+         throw new Error("cep não encontrado")
+      }
+
+      res.status(200).send(address)
+
+   } catch (error) {
+      res.status(400).send(error.message)
+   }
+}
